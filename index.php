bf2e4ec53df8a3159183f36fb618a1cd11d29503
بیژن خدابخش <36363538+beykhodabakhsh@users.noreply.github.com>
2018-05-13 01:24:47 +0430

    Create index.php

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="fa_IR">
+<head>
+    <link rel="stylesheet" href="style/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My HTML Page</title>
+</head>
+<body>
+<div id="wrapper">
+    <header id="head">
+        <div>
+            <img id="logo" src="images/bodo-logo911.png" alt="لوگو بدوگیم" title="bodogame logo">
+            <img id="banner" src="images/my.jpg" alt="بنر" title="banner">
+        </div>
+        <hr id="hr" />
+        <nav>
+            <ul>
+                <li><a href="#">خانه</a> </li>
+                <li><a href="#">ویژگی ها</a> </li>
+                <li><a href="#">قابلیت ها</a> </li>
+                <li><a href="#">درباره ما</a> </li>
+            </ul>
+        </nav>
+    </header>
+    <section id="main">
+
+        <article  id="content-image1" class="content-image1">
+            <p class="more"><a href="#">بیشتر بخوانید</a></p>
+            <img class="content-image" src="images/article.jpg" alt="تصویر مطلب" title="content-image">
+            <h1 class="title-content">مطلب اول</h1>
+            <p class="title-content">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. </p>
+
+        </article>
+
+        <article id="content-image2" class="content-image1">
+            <p class="more"><a href="#">بیشتر بخوانید</a></p>
+            <img class="content-image" src="images/article.jpg" alt="تصویر مطلب" title="content-image">
+            <h1 class="title-content">مطلب دوم</h1>
+            <p class="title-content">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. </p>
+
+        </article>
+
+        <article id="content-image3" class="content-image1">
+            <p class="more"><a href="#">بیشتر بخوانید</a></p>
+            <img class="content-image" src="images/article.jpg" alt="تصویر مطلب" title="content-image">
+            <h1 class="title-content">مطلب سوم</h1>
+            <p class="title-content">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. </p>
+
+        </article>
+
+    </section>
+</div>
+</body>
+</html>
